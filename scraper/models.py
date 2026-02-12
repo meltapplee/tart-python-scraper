e1771098e7c86912269dce6927dd261e70ba31f4
soohyeon <meltapplee@gmail.com>
2026-02-12 14:17:16 +0900

    feat: 컬럼 설명 추가 및 title, date 필드 타입 변경

--- scraper/models.py
+++ scraper/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 class Column(models.Model):
     id = models.AutoField(primary_key=True)
-    identifier = models.CharField(max_length=255)
-    title = models.TextField()
-    summary = models.TextField()
-    image = models.URLField()
-    date = models.TextField()
+    identifier = models.CharField("칼럼 아이디", max_length=255, unique=True)
+    title = models.CharField("칼럼 제목", max_length=255)
+    summary = models.TextField("칼럼 미리보기? 요약? 카드 밑에 나타나는 내용")
+    image = models.URLField("칼럼 이미지 URL")
+    date = models.DateField("발행 날짜")
+
+    class Meta:
+        db_table = "column"
