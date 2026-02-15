109b14bbe29ae478342bfeedae261f35f58f28d0
soohyeon <meltapplee@gmail.com>
2026-02-15 20:57:10 +0900

    refactor: Column 모델을 Article로 변경 및 필드 추가

--- scraper/models.py
+++ scraper/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
-class Column(models.Model):
+class Article(models.Model):
     id = models.AutoField(primary_key=True)
     identifier = models.CharField("칼럼 아이디", max_length=255, unique=True)
+    author = models.CharField("작성자", max_length=100)
     title = models.CharField("칼럼 제목", max_length=255)
-    summary = models.TextField("칼럼 미리보기? 요약? 카드 밑에 나타나는 내용")
-    image = models.URLField("칼럼 이미지 URL")
-    date = models.DateField("발행 날짜")
+    summary = models.TextField("본문 요약")
+    image = models.URLField("이미지 URL")
+    posted_at = models.DateField("발행 날짜")
+    url = models.URLField("원문 링크")
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        db_table = "column"
+        db_table = "article"
